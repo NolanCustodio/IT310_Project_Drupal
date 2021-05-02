c6f53d4f18931409d0ba6cb89cbcef755309d735
NolanCustodio <nac53@njit.edu>
2021-05-02 18:28:17 -0400

    initial commit

---
+++ web/sites/default/files/php/twig/608f0462132e6_uc-cart-empty.html.twig_Lw_xBvQNErhEIG58DeNMdxd_g/BTJeuDwqdSfRGyaI-x98d1_ZnGCRmlcjIepLkcSoGsM.php
@@ -0,0 +1,91 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/ubercart/uc_cart/templates/uc-cart-empty.html.twig */
+class __TwigTemplate_f820a1b4e90456766578689c2a3bc49a0f8d08c4791baf00d88835d5916485b7 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["trans" => 9];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['trans'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 8
+        echo "<p class=\"uc-cart-empty\">
+  ";
+        // line 9
+        echo t("There are no products in your shopping cart.", array());
+        // line 12
+        echo "</p>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/ubercart/uc_cart/templates/uc-cart-empty.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  60 => 12,  58 => 9,  55 => 8,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/ubercart/uc_cart/templates/uc-cart-empty.html.twig", "/opt/drupal/web/web/modules/ubercart/uc_cart/templates/uc-cart-empty.html.twig");
+    }
+}
---
+++ web/sites/default/files/php/twig/608f0462132e6_uc-payment-totals.html.tw_4am466p1xQ4RgwpQBOaZwXkiH/jh_iHx50rUOdLxGvOKP17HjG6jjj8uEI7xOCj684fA8.php
@@ -0,0 +1,111 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/ubercart/payment/uc_payment/templates/uc-payment-totals.html.twig */
+class __TwigTemplate_f81eb0d4835b7f3228dc4e266111fdbfbffb1b290d44ac75b3d8280bdc54c8f7 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["for" => 18];
+        $filters = ["escape" => 19];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 17
+        echo "<table class=\"uc-payment-totals\">
+  ";
+        // line 18
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["line_items"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["line"]) {
+            // line 19
+            echo "    <tr class=\"line-item-";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["line"], "type", [])), "html", null, true);
+            echo "\">
+      <td class=\"title\">";
+            // line 20
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["line"], "title", [])), "html", null, true);
+            echo ":</td>
+      <td class=\"price\">";
+            // line 21
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["line"], "amount", [])), "html", null, true);
+            echo "</td>
+    </tr>
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['line'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 24
+        echo "</table>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/ubercart/payment/uc_payment/templates/uc-payment-totals.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  80 => 24,  71 => 21,  67 => 20,  62 => 19,  58 => 18,  55 => 17,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/ubercart/payment/uc_payment/templates/uc-payment-totals.html.twig", "/opt/drupal/web/web/modules/ubercart/payment/uc_payment/templates/uc-payment-totals.html.twig");
+    }
+}
